100f0060102b38795a5b12c7293dece0f2d2b78a
bentannoy <bentannoy@protonmail.com>
2021-06-04 11:58:05 +0100

    added PostgreSQL variable and psycopg2

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+import os
+
 from datetime import timedelta
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # tell sql alchemy that the db lives at the root of the folder
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'meli'
 app.config['PROPAGATE_EXCEPTIONS'] = True
